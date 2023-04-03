6dde5c0b9e2e8e61751b8ee51e11a44c7b74022a
vinypaulino <vinydos3@gmail.com>
2023-04-03 15:46:59 -0300

    feat: init

---
+++ src/components/Mapping.tsx
@@ -0,0 +1,14 @@
+import { Grid } from "@material-ui/core";
+
+type Props = {
+  
+};
+
+export const Mapping = (props: Props) => {
+  return (
+    <Grid container>
+      <Grid item  >Formulario</Grid>
+      <Grid item > Mapa </Grid>
+    </Grid>
+  );
+};
